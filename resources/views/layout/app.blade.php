fb51e45d82c45de3eca495cef7a993106007f4e1
mdiqbalhossan <jmiqbal7265@gmail.com>
2024-10-10 21:25:01 +0600

    Logo and Favicon Update

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title') - SafeVax</title>
+    <link rel="icon" type="image/png" href="{{ asset('public/favicon.png') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" />
     @stack('styles')
